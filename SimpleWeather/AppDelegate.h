7643e8be8e488afa0e878b4958b09a2a70ea0fde
Andy Reyes <andy@andyjreyes.com>
2015-07-20 17:33:58 -0700

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Andy Reyes on 7/20/15.
+//  Copyright (c) 2015 Andy Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by Andy Reyes on 7/20/15.
+//  Copyright (c) 2015 Andy Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
