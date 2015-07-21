ffd21c76db364dfb8d789bc4b5f2d42434590f0c
Andy Reyes <andy@andyjreyes.com>
2015-07-20 18:01:18 -0700

    Added the WeatherItem class, meant to hold a single day's weather data. This class may or may not be used depending on the true requirements of this app.

---
+++ SimpleWeather/WeatherItem.m
@@ -0,0 +1,13 @@
+//
+//  WeatherItem.m
+//  SimpleWeather
+//
+//  Created by Andy Reyes on 7/20/15.
+//  Copyright (c) 2015 Andy Reyes. All rights reserved.
+//
+
+#import "WeatherItem.h"
+
+@implementation WeatherItem
+
+@end
